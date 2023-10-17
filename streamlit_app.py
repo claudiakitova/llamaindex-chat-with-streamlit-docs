eb0c4d3608b8facc1122ccb3b2718f0d68b1fa05
Claudia Kitova <claudia@kitova.me>
2023-10-17 14:54:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
-st.set_page_config(page_title="Chat with the Docs", page_icon="🐋", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="PhysicsTalk: Exploring the Universe through Conversation.", page_icon="🐋", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Docs")
+st.title("PhysicsTalk: Exploring the Universe through Conversation.")
 # st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
