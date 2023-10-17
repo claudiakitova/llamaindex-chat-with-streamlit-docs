d859de170d0b7614c54c8aba88b7f940049f7025
Claudia Kitova <claudia@kitova.me>
2023-10-17 16:32:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,19 +16,25 @@ st.set_page_config(page_title="PhysicsTalk",
                   )
 openai.api_key = st.secrets.openai_key
 st.title("PhysicsTalk")
-# st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.info("Contact me [there](https://kitova.me/about/)", icon="🐋")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question"}
+        {"role": "assistant",
+         "content": "Hey! Ask me a question."}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(
+          llm=OpenAI(
+            model="gpt-3.5-turbo", 
+            temperature=0.5, 
+            system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. 
+            Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
