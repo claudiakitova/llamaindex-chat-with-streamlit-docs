3269624e34acf7654160205cc5e9d957cb2bb20c
Claudia Kitova <claudia@kitova.me>
2023-10-18 11:56:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
+import pypdf
 
 st.set_page_config(page_title="PhysicsTalks", 
                    page_icon="☢️", layout="centered", 
